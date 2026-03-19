abad8ee39434f6caafb4b0c80e84a5cc2235bc5a
sjdkay <sjdkay@gmail.com>
2026-03-19 11:58:55 +0000

    Updated script. Just calls replica list for the DID, not file by file. Runs much faster.

--- files/DIDlist_Parse.sh
+++ files/DIDlist_Parse.sh
@@ -37,27 +37,15 @@ if ! grep -q DATASET $tmp_file; then
     exit 3
 fi
 
-# Checks passed, now process the file
-rucio did content list --short ${Scope}:${DID} > $tmp_file # Dump list of files in dataset to a file
 PathListFile="${Path}/${DID_Edit}_DID_Pathlist"
 # Need to switch this to name the DID pathlist nicely
 touch "${PathListFile}" # Open the file which will contain the path list
-NLines=$(wc --lines < $tmp_file) # Check number of files to process in total
-NLinesRed=$(( ((${NLines%.*}+5)/10)*10 )) # Round to nearest value of 10 to make the progress counter simpler
 
-echo "Processing ${NLines} files in dataset ${Scope}:${DID}$ and creating file with list of paths - ${PathListFile}."
-i=0
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    rucio replica list file --protocols root --pfns --rses isopenaccess $line >> ${PathListFile}
-    i=$(( $i + 1 ))
-    if [[ $(( $i % $(( ${NLinesRed}/10 )) )) == 0 ]]; then  # Go to the Microsoft school of progress tracking (aka, lying) and calculate progress
-	Prog=$(printf %.0f "$((10**2 * ${i}/${NLinesRed}))e-0")
-	echo "${Prog} % of file list processed"
-    fi
-done < $tmp_file
+rucio replica list file --protocols root --pfns --rses isopenaccess ${Scope}:${DID} >> ${PathListFile}
+
 echo "Parsed provided DID - $DID"
 echo "All files locations within this dataset have been printed to ${PathListFile}" # Need to automate this in future such that the file is named sensibly
-sleep 3
+sleep 1
 
 rm $tmp_file # Delete the tmp file
 
